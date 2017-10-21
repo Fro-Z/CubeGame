420c83d689e283173e4886a1708fc0f06ffc3978
Fro-Z <fro-z@email.cz>
2017-10-21 17:36:22 +0200

    Automatically load chunks around player

--- Assets/CubeWorld.cs
+++ Assets/CubeWorld.cs
@@ -8,22 +8,24 @@ using UnityEngine.Assertions;
 public class CubeWorld : MonoBehaviour {
 
 	public const int CUBE_SIZE = 2;
-	public const int viewDistance = 5;
+	public const int viewDistance = 4;
 
 	public Material chunkMaterial;
 	private WorldGenerator generator = new WorldGenerator();
+
+	Dictionary<Vector2Int, GameObject> loadedChunks = new Dictionary<Vector2Int, GameObject>();
 
 	void Start ()
 	{
 		for(int x = -viewDistance; x< viewDistance; x++)
 			for(int y = -viewDistance; y< viewDistance; y++)
 			{
-				CreateChunk(new Vector2Int(x, y));
+				LoadChunk(new Vector2Int(x, y));
 			}
 
 	}
 
-	void CreateChunk(Vector2Int chunkPos)
+	GameObject CreateChunk(Vector2Int chunkPos)
 	{
 		GameObject chunkObject = new GameObject("chunk", typeof(MeshFilter), typeof(MeshRenderer), typeof(Chunk), typeof(MeshCollider));
 
@@ -38,9 +40,62 @@ public class CubeWorld : MonoBehaviour {
 		// generate blocks
 		Chunk chunk = chunkObject.GetComponent<Chunk>();
 		generator.FillChunk(chunk, chunkPos);
+
+		return chunkObject;
 	}
 
-	void Update () {
-		
+	void Update ()
+	{
+		UpdateVisibleChunks();
 	}
+
+	private void UpdateVisibleChunks()
+	{
+		var cameraPos = Camera.main.gameObject.transform.position;
+		cameraPos /= (Chunk.CHUNK_SIZE * CUBE_SIZE); //transform to chunk coordinates
+
+		Vector2Int centerChunk = new Vector2Int((int)(cameraPos.x), (int)(cameraPos.z));
+
+		//find all chunks in an area around camera
+		HashSet<Vector2Int> visibleChunks = new HashSet<Vector2Int>();
+		for(int x = centerChunk.x - viewDistance; x< centerChunk.x + viewDistance; x++)
+			for (int y = centerChunk.y - viewDistance; y < centerChunk.y + viewDistance; y++)
+			{
+				visibleChunks.Add(new Vector2Int(x, y));
+			}
+
+		var chunksToRemove = new HashSet<Vector2Int>(loadedChunks.Keys);
+		chunksToRemove.ExceptWith(visibleChunks);
+		RemoveChunks(chunksToRemove);
+
+		var chunksToLoad = new HashSet<Vector2Int>(visibleChunks);
+		chunksToLoad.ExceptWith(loadedChunks.Keys);
+		LoadChunks(chunksToLoad);
+	}
+
+	private void RemoveChunks(HashSet<Vector2Int> chunksToRemove)
+	{
+		foreach (Vector2Int chunkPos in chunksToRemove)
+		{
+			GameObject chunk;
+			if(loadedChunks.TryGetValue(chunkPos,out chunk))
+			{
+				loadedChunks.Remove(chunkPos);
+				Destroy(chunk);
+			}
+		}
+	}
+
+	private void LoadChunks(HashSet<Vector2Int> chunksToLoad)
+	{
+		foreach (Vector2Int pos in chunksToLoad)
+			LoadChunk(pos);
+	}
+
+	void LoadChunk(Vector2Int pos)
+	{
+		loadedChunks.Add(pos, CreateChunk(pos));
+	}
+
+
 }
